64003c7573d0b048f94e1ca4042a4f61c009cf0c
Alex Gospodchikov <aleks-say@mail.ru>
2019-01-26 11:19:28 +0300

    Added some needed comments. Not sure if big-endian is broken, probably not.

--- test/util_tests.c
+++ test/util_tests.c
@@ -29,14 +29,17 @@ static void write_tests(void **state)
 	// Basic tests
 	// 11100011 11001100 01100101 10110110
 	lword_t x = 3821823414;
+	// Write the last byte
 	ret = write_data(erric, 10, (uint8_t*)&x, 1);
 	assert_int_equal(erric->memory[10], 182);
 	assert_int_equal(ret, 0);
 
+	// Write the last 2 bytes
 	ret = write_data(erric, 10, (uint8_t*)&x, 2);
 	assert_int_equal(erric->memory[10], 26038);
 	assert_int_equal(ret, 0);
 
+	// Write the whole 4 bytes
 	ret = write_data(erric, 15, (uint8_t*)&x, 4);
 	assert_int_equal(erric->memory[15], 58316);
 	assert_int_equal(erric->memory[16], 26038);
@@ -80,14 +83,17 @@ static void read_tests(void **state)
 	// Basic cases
 	// 11100011 11001100 01100101 10110110
 	lword_t x = 3821823414;
+	// Write all bytes at once
 	write_data(erric, 0, (uint8_t*)&x, 4);
 	ret = read_sword(erric, 0);
-	// Byte 2 of the original number
+	// Read the end of the first word
 	assert_int_equal(ret, 204);
 
+	// Read the first word
 	ret = read_word(erric, 0);
 	assert_int_equal(ret, 58316);
 
+	// Read 2 words and assemble them into a 32-bit number
 	ret = read_lword(erric, 0);
 	assert_int_equal(ret, 3821823414);
 
